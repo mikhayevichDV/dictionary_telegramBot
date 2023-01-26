dc7809229e3b66736280f508046bbb2ca1c80357
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-01-27 00:40:04 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Telegraf } = require('telegraf');
+const axios = require("axios");
+
+const bot = new Telegraf('5866857954:AAE1VkS-AHZVjf6dnuaD7lzYrObzKweiTME');
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.on('message', async (ctx)=>{
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${ctx.message.text}`
+    const response = await axios.get(url);
+    ctx.reply(`Твой ответ - `)
+    const reply = response.data.map(elem=>elem.meanings[0].definitions.map((elem)=> ctx.reply(`${elem.definition}`) ))
+    console.log(reply)
+    })
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));

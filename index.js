7a1cb74db34fadaf8d68f1f2df929f6d46135b6d
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-01-27 11:43:20 +0300

    feat: add errors catcher

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const { Telegraf } = require('telegraf');
+const {Telegraf} = require('telegraf');
 const axios = require("axios");
 
 const bot = new Telegraf('5812635735:AAF8vZhD-J4356RrHPEXzw-8iuEwRyuJYKU');
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.on('message', async (ctx)=>{
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${ctx.message.text}`
-    const response = await axios.get(url);
-    ctx.reply(`Твой ответ - `)
-    const reply = response.data.map(elem=>elem.meanings[0].definitions.map((elem)=> ctx.reply(`${elem.definition}`) ))
-    console.log(reply)
-    })
+bot.start((ctx) => ctx.reply('Hello!'));
+bot.on('message', async (ctx) => {
+   try{
+       const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${ctx.message.text}`
+        const response = await axios.get(url);
+       console.log(response)
+           ctx.reply(`Your answer(s): `)
+           response.data.map(elem => elem.meanings[0].definitions.map((elem) => ctx.reply(`${elem.definition}`)))
+   } catch (e) {
+       console.error(e)
+       ctx.reply('No definition found')
+   }
+})
 bot.launch();
 
-// Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
